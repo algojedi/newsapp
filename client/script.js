c3dbbe3a2b83f6526c0e810566d04a0d1700f810
banwsukh <ssbanwait78@gmail.com>
2020-07-03 21:28:42 -0400

    responsive articles

--- client/script.js
+++ client/script.js
@@ -5,69 +5,56 @@ const numOfArticlesToDisplay = 7
 const state = {
     cardFlipped: false,
     isLoading: false,
+    // variables to track whether re-fetching is needed
+    ca: false,
+    au: false,
+    us: false,
+    in: false,
+    gb: false
 }
 
-const url = 'http://localhost:3001/main?id=ca'
-const btnClicked = () => {
-    console.log('clicked btn')
-    info.innerHTML = 'john lennon'
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', '/main?id=au', true)
-
-    xhr.onprogress = function () {
-        info.innerHTML = '<p>...loading</p>'
-    }
-    xhr.onerror = function () {
-        info.innerHTML = '<h4>What a shame.. there was an error of sorts</h4>'
-    }
-    xhr.onload = function () {
-        if (this.status == 200) {
-            console.log(this.responseText)
-            displayResults(this.responseText)
-        }
-    }
-
-    xhr.send()
-}
-
-// results is an array of article objects
-const displayResults = (results) => {
-    results.forEach((element) => {})
-}
-
-// ----------------- functionality for the cards in projects section
-const cardClicked = (element) => {
-    element.classList.toggle('flipped')
-    console.log(element)
-    console.log(element.getAttribute('name'))
-    const cardName = element.getAttribute('name')
+const flipCard = (element) => {
+    const country = element.getAttribute('name')
+    const parentElement = document.getElementById(country)
+    console.log('parent element, ', parentElement)
+    parentElement.classList.toggle('flipped')
+    const cardName = parentElement.getAttribute('name')
+    console.log('name given by flip card: ', cardName)
     getArticles(cardName)
 }
 
 const getArticles = (name) => {
+    if (state[name]) {
+        console.log('not reloading ', state[name])
+        return 
+    } 
+    state[name] = true // indicates country info has been fetched
+    console.log('name being passed to url: ', name)
     const url = `/main?id=${name}`
-    const xhr = new XMLHttpRequest()
 
+    const xhr = new XMLHttpRequest()
+    console.log('about to make http req')
     xhr.open('GET', url, true)
 
     xhr.onprogress = function () {
         // info.innerHTML = '<p>...loading</p>'
         state.isLoading = true
+
     }
     xhr.onerror = function () {
-        // info.innerHTML = '<h4>What a shame.. there was an error of sorts</h4>'
         state.isLoading = false
-        return null
+        console.log('apparently there was a network error')
     }
     xhr.onload = function () {
         state.isLoading = false
         if (this.status == 200) {
             // console.log(this.responseText)
+            console.log('made http req')
             const result = JSON.parse(this.responseText)
             const topArticles = result.filter((el, i) => {
                 return i < numOfArticlesToDisplay
             })
-            console.log(topArticles)
+            console.log({ topArticles })
             dispalyArticles(topArticles, name)
         }
     }
@@ -78,15 +65,17 @@ const dispalyArticles = (articles, name) => {
     if (!articles) return
     let toDisplay = ''
     articles.forEach((article) => {
-        console.log(article.urlToImage)
+        // console.log(article.urlToImage)
         toDisplay += ` 
         <div class="article"> 
-            <img class="article-photo" src=${article.urlToImage}>
-            <h2 class='article-title'>${article.title}</h2>
-            <p class='article-date'>${article.publishedAt.slice(0, 10)}</p>
+            <div class='img-wrapper'>
+                <img class="article-photo" src=${article.urlToImage}>
+            </div>
+            <a class='article-link' href=${article.url} target="_blank">
+                <h2 class='article-title'>${article.title}</h2>
+            </a> 
             <p class='article-desc'>${article.description}</p>
-            <a class='article-btn-link' href=${article.url} target="_blank">Link to article</a> 
-            <hr>
+            <hr class='article-separator'>
         </div>
         `
     })
@@ -97,22 +86,21 @@ const dispalyArticles = (articles, name) => {
     // cdaArticles.innerHTML = toDisplay
 }
 
-const findElement = name => {
+const findElement = (name) => {
     switch (name) {
         case 'ca':
-            return document.getElementById('can-articles') 
+            return document.getElementById('can-articles')
         case 'us':
-            return document.getElementById('us-articles') 
-        case 'uk':
-            return document.getElementById('uk-articles') 
+            return document.getElementById('us-articles')
+        case 'gb':
+            return document.getElementById('uk-articles')
         case 'au':
-            return document.getElementById('au-articles') 
+            return document.getElementById('au-articles')
         case 'in':
-            return document.getElementById('in-articles') 
+            return document.getElementById('in-articles')
         default:
             return null
     }
 }
-
 
 // country codes: au, ca, us, gb, in
